f30f9c925af5e69334ab1f7838f40ce57ee53056
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2017-12-28 22:49:59 +0000

    added gtk3 port to SVN

---
+++ gtkwave3-gtk3/src/gtk23compat.h
@@ -0,0 +1,49 @@
+#ifndef WAVE_GTK23COMPAT_H
+#define WAVE_GTK23COMPAT_H
+
+/* this is for any future gtk2/gtk3 compatibility */
+
+#ifdef MAC_INTEGRATION
+/* #undef WAVE_ALLOW_QUARTZ_FLUSH_WORKAROUND */
+#endif
+
+#if GTK_CHECK_VERSION(3,0,0)
+/* workaround for wave_vslider not rendering properly on startup */
+#define WAVE_ALLOW_GTK3_VSLIDER_WORKAROUND
+#define WAVE_GTK3_HIERSEARCH_DEBOUNCE
+#endif
+
+#define WAVE_GTKIFE(a,b,c,d,e) {a,b,c,d,e,NULL}
+#define WAVE_GDK_GET_POINTER(a,b,c,bi,ci,d)  gdk_window_get_pointer(a,bi,ci,d)
+#define WAVE_GDK_GET_POINTER_COPY x=xi; y=yi;
+
+#define WAVE_GTK_SFUNCAST(x) ((void (*)(GtkWidget *, gpointer))(x))
+
+/* gtk3->4 deprecated */
+
+#if GTK_CHECK_VERSION(3,0,0)
+
+#define YYY_GTK_TREE_VIEW GTK_SCROLLABLE
+#define YYY_gtk_tree_view_get_vadjustment gtk_scrollable_get_vadjustment
+#define YYY_gtk_tree_view_get_hadjustment gtk_scrollable_get_hadjustment
+#define YYY_gtk_tree_view_set_vadjustment gtk_scrollable_set_vadjustment
+#define YYY_gtk_tree_view_set_hadjustment gtk_scrollable_set_hadjustment
+
+#define YYY_GTK_TEXT_VIEW GTK_SCROLLABLE
+#define YYY_gtk_text_view_get_vadjustment gtk_scrollable_get_vadjustment
+
+#else
+
+#define YYY_GTK_TREE_VIEW GTK_TREE_VIEW
+#define YYY_gtk_tree_view_get_vadjustment gtk_tree_view_get_vadjustment
+#define YYY_gtk_tree_view_get_hadjustment gtk_tree_view_get_hadjustment
+#define YYY_gtk_tree_view_set_vadjustment gtk_tree_view_set_vadjustment
+#define YYY_gtk_tree_view_set_hadjustment gtk_tree_view_set_hadjustment
+
+#define YYY_GTK_TEXT_VIEW GTK_TEXT_VIEW
+#define YYY_gtk_text_view_get_vadjustment gtk_text_view_get_vadjustment
+
+#endif
+
+#endif
+
